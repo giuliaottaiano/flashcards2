701c89be4044d1793f5d069c578cc7463aa8c750
giuliaottaiano <ottaianogiulia22@gmail.com>
2024-11-26 21:46:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 6 fatorial?',
+    '6 fatorial é 720'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 10 elevado a 3?',
+    '10 elevado a 3 é 1000'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz nome em inglês?',
+    'nome em inglês é name'
+)
